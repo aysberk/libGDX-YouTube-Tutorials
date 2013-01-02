c8a602d66ffbb4563defee00932000f95360432b
derSchotte <kontakt@frank-mcdonald.de>
2013-01-02 16:17:00 +0100

    Grundeinstellungen der Main.java, MainActivity.java und MainTutorials.java... Eingestellt wurde Fenstergröße, Titel sowie Version und useGL20=true für Desktop und Android.

--- Tutorials-android/src/de/derSchotte/tutorials/MainActivity.java
+++ Tutorials-android/src/de/derSchotte/tutorials/MainActivity.java
@@ -6,12 +6,18 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class MainActivity extends AndroidApplication {
-    @Override
+	
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 * 
+	 * cfg.use20 = true um Texturen zu verwenden welche nicht PowerOf2 verwenden ( z.b. 8x8, 16x16, 32x32 usw. )
+	 */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        cfg.useGL20 = false;
+        cfg.useGL20 = true;
         
         initialize(new MainTutorials(), cfg);
     }
--- Tutorials-desktop/src/de/derSchotte/tutorials/Main.java
+++ Tutorials-desktop/src/de/derSchotte/tutorials/Main.java
@@ -4,12 +4,23 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
+	
+	/*
+	 * MainTutorials.TITLE / .VERSION / .windowWidth / .windowHeight
+	 * Diese werden in der MainTutorials.java bereitgestellt und können hier verwendet werden.
+	 * cfg.fullscreen und ccfg.resizeable sind auf false gesetzt damit das Fenster nicht in Fullscreen gesetzt werden kann.
+	 * resizeable auf false damit das Fenster in seiner größe nicht verändert werden kann.
+	 * 
+	 *  cfg.use20 = true um Texturen zu verwenden welche nicht PowerOf2 verwenden ( z.b. 8x8, 16x16, 32x32 usw. )
+	 */
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "Tutorials";
-		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.title = MainTutorials.TITLE + MainTutorials.VERSION;
+		cfg.useGL20 = true;
+		cfg.width = MainTutorials.windowWidth;
+		cfg.height = MainTutorials.windowHeight;
+		cfg.fullscreen = false;
+		cfg.resizable = false;
 		
 		new LwjglApplication(new MainTutorials(), cfg);
 	}
